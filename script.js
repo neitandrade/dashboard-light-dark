650e9ee63eba5c0068805fd3693faf28c5e41e00
natan <106288948+neitandrade@users.noreply.github.com>
2024-06-27 16:16:44 +0000

    Att Dark/Ligh Button

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const themeChange = document.querySelector(".theme-change")
 //change theme
 themeChange.addEventListener('click', () =>{
     document.body.classList.toggle('dark-theme-variables');
+
+    themeChange.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeChange.querySelector('span:nth-child(2)').classList.toggle('active'); 
+    /* TOGGLE REMOVE E ADICIONA QUANDO O EVENTO É ACIONADO */
+
 })
